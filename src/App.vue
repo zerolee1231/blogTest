3997ae345dc0c623934d5be4a37258ddea858183
zerolee <lizelong@fuzhi.ai>
2023-11-01 17:17:38 +0800

    committest

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="w-full max-w-screen-xl p-4">
+      <!-- Page Title -->
+      <h1 class="text-3xl font-semibold text-center mb-8">Pricing Plans</h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <!-- Pricing Cards -->
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <!-- Basic Plan -->
+        <a-card :bordered="false" class="shadow-md">
+          <template #title>
+            <h2 class="text-2xl font-semibold">Basic</h2>
+          </template>
+          <p class="text-3xl font-semibold text-primary">$10/month</p>
+          <a-divider></a-divider>
+          <ul class="text-gray-600">
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+          </ul>
+          <a-button type="primary" class="mt-4">Select Plan</a-button>
+        </a-card>
+
+        <!-- Pro Plan -->
+        <a-card :bordered="false" class="shadow-md">
+          <template #title>
+            <h2 class="text-2xl font-semibold">Pro</h2>
+          </template>
+          <p class="text-3xl font-semibold text-primary">$20/month</p>
+          <a-divider></a-divider>
+          <ul class="text-gray-600">
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+          </ul>
+          <a-button type="primary" class="mt-4">Select Plan</a-button>
+        </a-card>
+
+        <!-- Premium Plan -->
+        <a-card :bordered="false" class="shadow-md">
+          <template #title>
+            <h2 class="text-2xl font-semibold">Premium</h2>
+          </template>
+          <p class="text-3xl font-semibold text-primary">$30/month</p>
+          <a-divider></a-divider>
+          <ul class="text-gray-600">
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+            <li>Feature 5</li>
+          </ul>
+          <a-button type="primary" class="mt-4">Select Plan</a-button>
+        </a-card>
+      </div>
+    </div>
+  </div>
+</template>
